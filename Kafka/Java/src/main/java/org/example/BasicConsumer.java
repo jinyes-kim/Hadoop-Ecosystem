f6e1550f87be408c3528785134d0778de46e0e86
jinyes-kim <jinung@kakao.com>
2021-08-18 21:35:45 +0900

    Add Kafka Java Producer & Consumer

---
+++ Kafka/Java/src/main/java/org/example/BasicConsumer.java
@@ -0,0 +1,54 @@
+package org.example;
+
+import org.apache.kafka.clients.consumer.*;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.errors.WakeupException;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+
+public class BasicConsumer {
+    private final static Logger logger = LoggerFactory.getLogger(BasicConsumer.class);
+    private final static String TOPIC_NAME = "jinyes";
+    private final static String BOOTSTRAP_SERVERS = "localhost:9092";
+    private final static String GROUP_ID = "group_1";
+    private static KafkaConsumer<String, String> consumer;
+
+    public static void main(String[] args) {
+        Runtime.getRuntime().addShutdownHook(new ShutdownThread());
+
+        Properties configs = new Properties();
+        configs.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+        configs.put(ConsumerConfig.GROUP_ID_CONFIG, GROUP_ID);
+        configs.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        configs.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+
+        consumer = new KafkaConsumer<>(configs);
+        consumer.subscribe((Arrays.asList(TOPIC_NAME)));
+
+        try {
+            while (true) {
+                ConsumerRecords<String, String> records = consumer.poll(Duration.ofSeconds(1));
+                for (ConsumerRecord<String, String> record : records) {
+                    logger.info("record: {}", record);
+                }
+            }
+        } catch (WakeupException e) {
+            logger.warn("Wakeup Consumer");
+        } finally {
+            consumer.close();
+        }
+    }
+
+    static class ShutdownThread extends Thread {
+        public void run() {
+            logger.info("Shutdown hock");
+            consumer.wakeup();
+        }
+    }
+
+}
