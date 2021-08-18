19a5fa48b8233e53e52bf2e8c963b1a39c01e6c9
jinyes-kim <jinung@kakao.com>
2021-08-18 20:16:58 +0900

    Update Basic Kafka python Consumer & Producer

--- Kafka/Python/basic_consumer.py
+++ Kafka/Python/basic_consumer.py
@@ -4,20 +4,19 @@ from kafka import KafkaConsumer
 broker = ['localhost:9092']
 
 # Topic name
-topic = 'jinyes'
+topic = "jinyes"
 
 # Generate consumer object
 consumer = KafkaConsumer(topic,
-                         group_id='group_123',
+                         group_id="group_1",
                          bootstrap_servers=broker,
-                         auto_offset_reset='earliest')
+                         auto_offset_reset="earliest")
 
 # Read message from consumer object
 try:
     for msg in consumer:
         print("Topic: {}\nPartition: {}\nOffset: {}\nKey: {}\nValue: {}\n".format(
-            msg.topic, msg.partition, msg.offset, msg.key, msg.value.decode('utf-8')
-        ))
+            msg.topic, msg.partition, msg.offset, msg.key, msg.value.decode("utf-8")))
 
 except KeyboardInterrupt:
     exit(0)
--- Kafka/Python/basic_producer.py
+++ Kafka/Python/basic_producer.py
@@ -1,10 +1,10 @@
 from kafka import KafkaProducer
 
 # Kafka Broker IP
-broker = ['192.168.0.2:9092']
+broker = ["192.168.0.2:9092"]
 
 # Topic name
-topic = 'jinyes'
+topic = "jinyes"
 
 # Generate producer object
 producer = KafkaProducer(bootstrap_servers=broker)
@@ -13,5 +13,5 @@ producer = KafkaProducer(bootstrap_servers=broker)
 # Send test message
 for i in range(100):
     msg = "Hello Kafka {}".format(i)
-    producer.send(topic, msg.encode('utf-8'))
+    producer.send(topic, msg.encode("utf-8"))
     producer.flush()
